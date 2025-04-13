0bfe24306903f6af47721e01cbfaa60d63a7a6d6
Redstoner <85954818+Redstoner-2019@users.noreply.github.com>
2025-04-13 23:31:04 +0200

    done nothing

--- src/main/java/me/redstoner2019/statserver/stats/StatsController.java
+++ src/main/java/me/redstoner2019/statserver/stats/StatsController.java
@@ -48,7 +48,7 @@ public class StatsController {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                init();
+                //init();
                 logger.log(Level.INFO,"Init done");
             }
         }).start();
